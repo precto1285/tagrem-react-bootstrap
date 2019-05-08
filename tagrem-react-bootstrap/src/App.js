a92fc998e8a8cbf55ae6dc1a805993c459151b62
Philippe Recto <precto1285@gmal.com>
2019-05-07 21:12:33 -0700

    created jumbotron with image

--- tagrem-react-bootstrap/src/App.js
+++ tagrem-react-bootstrap/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import './App.css';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       Hello World!
     </div>
   );
---
+++ tagrem-react-bootstrap/src/components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
+import BannerImage from '../images/banner.jpg';
+import './banner.css';
+
+const Banner = () => {
+  return (
+    <div>
+      <Jumbotron className="jumbotron" />
+    </div>
+
+  );
+};
+
+export default Banner;
