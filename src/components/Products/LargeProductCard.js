4bdb4751a846c40552a03812f205df56b145d894
Philippe Recto <precto1285@gmal.com>
2019-05-09 03:12:58 -0700

    added sprite icons and animated

--- src/components/Products/LargeProductCard.js
+++ src/components/Products/LargeProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'reactstrap';
+import "./LargeProductCard.css"
 
 const Product1 = (props) => {
   return (
@@ -13,7 +14,43 @@ const Product1 = (props) => {
         <br />
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero debitis, ipsa fugiat inventore et modi. Temporibus
           hic culpa, commodi tenetur animi ea. Deserunt illum iure consectetur ipsa ullam! Saepe quasi sunt ad deleniti,
-          quibusdam voluptate quis doloribus suscipit velit aliquam magni asperiores illo accusantium neque, nemo porro, voluptatum commodi illum.</p></Col>
+          quibusdam voluptate quis doloribus suscipit velit aliquam magni asperiores illo accusantium neque, nemo porro, voluptatum commodi illum.</p>
+          <br />
+          <Row>
+            <Col className="col-sm-2">
+              <span id="oneIcon"></span>
+            </Col>
+            <Col className="col-sm-10">
+              <p>Lorem ipsum dolor sit amet</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col className="col-sm-2">
+              <span id="twoIcon"></span>
+            </Col>
+            <Col className="col-sm-10">
+              <p> ipsa fugiat inventore et</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col className="col-sm-2">
+              <span id="threeIcon"></span>
+            </Col>
+            <Col className="col-sm-10">
+              <p> Saepe quasi sunt</p>
+            </Col>
+          </Row>
+
+
+
+
+
+
+
+
+        </Col>
         <Col className="col-sm-6"><div className="mb-3">{props.image}</div></Col>
       </Row>
 
